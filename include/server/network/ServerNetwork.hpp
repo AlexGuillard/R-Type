2121724c6400ec8db38b3add11ca697480c6c16d
Benjamin <benjamin.colimard@epitech.eu>
2023-09-25 17:56:00 +0200

    feat: put boost asio on server for ioService

--- include/server/network/ServerNetwork.hpp
+++ include/server/network/ServerNetwork.hpp
@@ -8,20 +8,21 @@
 #ifndef SERVERNETWORK_HPP_
     #define SERVERNETWORK_HPP_
 	#include <iostream>
+	#include <boost/asio.hpp>
     #include "ANetwork.hpp"
 
 namespace Network {
     class ServerNetwork : public ANetwork {
         public:
-            ServerNetwork(std::string hostname, int port);
+            ServerNetwork(boost::asio::io_service& io_service, int port);
             ~ServerNetwork();
 			void receive();
-			static void handle(const asio::error_code& error, std::size_t bytes_transferred);
+			void send();
+			// static void handle(const asio::error_code& error, std::size_t bytes_transferred);
         protected:
-			asio::io_context io_context;
-			asio::ip::udp::socket _socket;
-			asio::ip::udp::endpoint _endpoint;
-			char *_data[1024];
+			boost::asio::ip::udp::socket _socket;
+			boost::asio::ip::udp::endpoint _endpoint;
+			char _data[1024];
         private:
     };
 }
--- src/server/network/ServerNetwork.cpp
+++ src/server/network/ServerNetwork.cpp
@@ -7,8 +7,10 @@
 
 #include "server/network/ServerNetwork.hpp"
 
-Network::ServerNetwork::ServerNetwork(std::string hostname, int port) : _socket(io_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), port))
+Network::ServerNetwork::ServerNetwork(boost::asio::io_service& io_service, int port)
+	: _socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port))
 {
+	receive();
 }
 
 Network::ServerNetwork::~ServerNetwork()
@@ -17,11 +19,27 @@ Network::ServerNetwork::~ServerNetwork()
 
 void Network::ServerNetwork::receive()
 {
-	_socket.async_receive_from(asio::buffer(_data, 1024), _endpoint, handle);
-	std::cout << _data << std::endl;
+	_socket.async_receive_from(boost::asio::buffer(_data, 1024), _endpoint,
+    [this] (boost::system::error_code error, std::size_t recvd_bytes) {
+        if ( !error && recvd_bytes > 0 ) {
+            std::cout << "[" << recvd_bytes << "] " << _data << std::endl;
+            send();
+        }
+        else {
+            receive();
+        }
+    });
 }
 
-void Network::ServerNetwork::handle(const asio::error_code& error, std::size_t bytes_transferred)
+void Network::ServerNetwork::send()
 {
-    std::cout << "Received: '" << bytes_transferred << "bytes from server" << "'\n";
+	std::string str = "endpoint: ";
+
+	str += _endpoint.address().to_string();
+	str += " port ";
+	str += std::to_string((int)_endpoint.port());
+    _socket.async_send_to(boost::asio::buffer(str.c_str(), str.length()), _endpoint,
+	[this] (boost::system::error_code error, std::size_t recvd_bytes) {
+	    receive();
+	});
 }
