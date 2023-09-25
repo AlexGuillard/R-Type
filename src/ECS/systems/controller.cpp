d12d139f6cc3b1645f2d5039acf29b1fc327e23d
azeuio <nicolas.mesrine@epitech.eu>
2023-09-25 16:18:58 +0200

    refactor(System): move all the code about moving the player in a function

--- src/ECS/systems/controller.cpp
+++ src/ECS/systems/controller.cpp
@@ -17,10 +17,12 @@ namespace ECS::systems {
 	template <typename T>
 	static T clamp(T value, T min, T max)
 	{
-		if (value < min)
+		if (value < min) {
 			return min;
-		if (value > max)
+		}
+		if (value > max) {
 			return max;
+		}
 		return value;
 	}
 
@@ -30,40 +32,61 @@ namespace ECS::systems {
 		return value < 0 ? -value : value;
 	}
 
+	/**
+	 * @brief Change the velocity of an entity based on the player input
+	 * @param velocity Current velocity of the entity
+	 * @param acceleration Number of frames needed to reach max speed
+	 * @param maxSpeed Maximum speed of the entity
+	 * @param
+	 */
+	static void changeVelocity(
+		ECS::components::VelocityComponent &velocity,
+		ECS::components::ControllableComponent &controllable,
+		float nbFrameToMaxSpeed,
+		float nbFrameToStop,
+		float maxSpeed
+	)
+	{
+		const float acceleration = maxSpeed / nbFrameToMaxSpeed;
+		const float deceleration = maxSpeed / nbFrameToStop;
+
+		if (IsKeyDown(controllable.up)) {
+			velocity.y -= 1 * acceleration;
+		} else if (IsKeyDown(controllable.down)) {
+			velocity.y += 1 * acceleration;
+		} else {
+			if (abs(velocity.y) < deceleration) {
+				velocity.y = 0;
+			} else {
+				velocity.y += (velocity.y > 0 ? -1 : 1) * deceleration;
+			}
+		}
+		if (IsKeyDown(controllable.left)) {
+			velocity.x -= 1 * acceleration;
+		} else if (IsKeyDown(controllable.right)) {
+			velocity.x += 1 * acceleration;
+		} else {
+			if (abs(velocity.x) < deceleration) {
+				velocity.x = 0;
+			} else {
+				velocity.x += (velocity.x > 0 ? -1 : 1) * deceleration;
+			}
+		}
+		velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed);
+		velocity.y = clamp(velocity.y, -maxSpeed, maxSpeed);
+	}
+
 	void controller(
 		[[maybe_unused]] ECS::containers::Registry &registry,
 		ECS::containers::SparseArray<ECS::components::VelocityComponent> &velocities,
 		ECS::containers::SparseArray<ECS::components::ControllableComponent> &controllables)
 	{
-		const float MAX_SPEED = 500;
-		const float NB_FRAME_TO_MAX_SPEED = 5;
-		const float NB_FRAME_TO_STOP = 5;
-		const float ACCELERATION = MAX_SPEED / NB_FRAME_TO_MAX_SPEED;
-		const float DECELERATION = MAX_SPEED / NB_FRAME_TO_STOP;
+		const float maxSpeed = 500;
+		const float nbFrameToMaxSpeed = 5;
+		const float nbFrameToStop = 5;
 
 		for (auto &&[velocity, controllable] : ECS::containers::Zipper(velocities, controllables)) {
-			if (IsKeyDown(controllable->up))
-				velocity->y -= 1 * ACCELERATION;
-			else if (IsKeyDown(controllable->down))
-				velocity->y += 1 * ACCELERATION;
-			else {
-				if (abs(velocity->y) < DECELERATION)
-					velocity->y = 0;
-				else
-					velocity->y += (velocity->y > 0 ? -1 : 1) * DECELERATION;
-			}
-			if (IsKeyDown(controllable->left))
-				velocity->x -= 1 * ACCELERATION;
-			else if (IsKeyDown(controllable->right))
-				velocity->x += 1 * ACCELERATION;
-			else {
-				if (abs(velocity->x) < DECELERATION)
-					velocity->x = 0;
-				else
-					velocity->x += (velocity->x > 0 ? -1 : 1) * DECELERATION;
-			}
-			velocity->x = clamp(velocity->x, -MAX_SPEED, MAX_SPEED);
-			velocity->y = clamp(velocity->y, -MAX_SPEED, MAX_SPEED);
+			changeVelocity(*velocity, *controllable, nbFrameToMaxSpeed, nbFrameToStop, maxSpeed);
 		}
 	}
 
