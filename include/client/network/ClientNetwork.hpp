c0550ff2d9d102e0a55eb901748f4e87be3a476f
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-09-26 18:33:11 +0200

    feat: add ClientNetwork basic function

--- include/client/network/ClientNetwork.hpp
+++ include/client/network/ClientNetwork.hpp
@@ -8,15 +8,21 @@
 #ifndef CLIENTNETWORK_HPP_
     #define CLIENTNETWORK_HPP_
     #include "ANetwork.hpp"
+	#include <iostream>
 
 namespace Network {
-    class ClientNetwork : public ANetwork  {
-        public:
-            ClientNetwork();
-            ~ClientNetwork();
 
-        protected:
+    class ClientNetwork : public ANetwork  {
+
+        public:
+            ClientNetwork(int port, std::string machine);
+            ~ClientNetwork() override;
+			void send(const std::string& message) override;
+
         private:
+			int _port;
+			std::string _host;
+			int _fdClient;
     };
 }
 
--- src/client/network/ClientNetwork.cpp
+++ src/client/network/ClientNetwork.cpp
@@ -7,6 +7,24 @@
 
 #include "client/network/ClientNetwork.hpp"
 
-Network::ClientNetwork::ClientNetwork() {}
+Network::ClientNetwork::ClientNetwork(int port, std::string machine) : _port(port), _host(machine), _fdClient(-1)
+{
+}
 
-Network::ClientNetwork::~ClientNetwork() {}
+Network::ClientNetwork::~ClientNetwork()
+{
+	if (_fdClient == -1) {
+		close(_fdClient);
+	}
+}
+
+void Network::ClientNetwork::send(const std::string& message)
+{
+	if (_fdClient == -1) {
+		throw Error("Error:client not connected");
+	}
+
+	if (::send(_fdClient, message.c_str(), message.size(), 0) == -1) {
+		throw Error("Error: failed to send message");
+	}
+}
