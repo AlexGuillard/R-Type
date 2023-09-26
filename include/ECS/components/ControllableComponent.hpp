941cf06e631275d3c5b7d24f1f108a31a0f097f3
azeuio <nicolas.mesrine@epitech.eu>
2023-09-26 10:55:43 +0200

    feat(Component): add missing properties to the controllable component

--- include/ECS/components/ControllableComponent.hpp
+++ include/ECS/components/ControllableComponent.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <raylib.h>
+#include <cstddef>
 
 namespace ECS::components {
 	/**
@@ -16,11 +17,17 @@ namespace ECS::components {
 	 * @param down Key to go down
 	 * @param left Key to go left
 	 * @param right Key to go right
+	 * @param fire Key to fire
+	 * @param force Key to use force
+	 * @param timeFireButtonHeld Time (in sec) since the fire button is held
 	 */
 	struct ControllableComponent {
 		KeyboardKey up = KEY_UP;
 		KeyboardKey down = KEY_DOWN;
 		KeyboardKey left = KEY_LEFT;
 		KeyboardKey right = KEY_RIGHT;
+		KeyboardKey fire = KEY_SPACE;
+		KeyboardKey force = KEY_LEFT_SHIFT;
+		double timeFireButtonHeld = 0.0;
 	};
 } // namespace ECS::components
