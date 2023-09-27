801c98ee62f56d1c819decfdc1660a3e56aadc24
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-09-27 16:11:55 +0200

    feat: add doc to the function of display

--- include/client/display/Display.hpp
+++ include/client/display/Display.hpp
@@ -12,19 +12,64 @@
 	#include <raylib.h>
 
 namespace Screen {
+	/**
+	 * @brief Display contain fonction for the graphic of the client
+	 *
+	 * */
     class Display {
         public:
+			/**
+			 * @brief Init the window with raylib
+			 *
+			 */
 			static void initWindow();
+			/**
+			 * @brief loop display window with menu or game
+			 *
+			 */
 			void displayWindow();
 
+			/**
+			 * @brief display an input for hostname
+			 *
+			 */
 			void displayHostNameInput();
+			/**
+			 * @brief display an input for port
+			 *
+			 */
 			void displayPortInput();
+			/**
+			 * @brief display button for connection
+			 *
+			 */
 			void displayConnectionButton();
+			/**
+			 * @brief detect the action in menu
+			 *
+			 */
 			void detectActionMenu();
+			/**
+			 * @brief do when action is mouse clicked in menu
+			 *
+			 */
 			void mouseClickedMenu();
+			/**
+			 * @brief detect the key pressed for the input zone in menu
+			 *
+			 * @param KeyPressed
+			 */
 			void keyPressededMenu(int KeyPressed);
+			/**
+			 * @brief draw the menu
+			 *
+			 */
 			void drawMenu();
 
+			/**
+			 * @brief draw the game
+			 *
+			 */
 			void drawGame();
 
 
