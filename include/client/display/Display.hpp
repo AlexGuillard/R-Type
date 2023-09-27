852bca68808a1990966043c4476a162d509d70c4
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-09-27 15:56:28 +0200

    feat: Add function Display

--- include/client/display/Display.hpp
+++ include/client/display/Display.hpp
@@ -8,14 +8,38 @@
 #ifndef DISPLAY_HPP_
     #define DISPLAY_HPP_
 
+	#include <iostream>
+	#include <raylib.h>
+
 namespace Screen {
     class Display {
         public:
+			static void initWindow();
+			void displayWindow();
+
+			void displayHostNameInput();
+			void displayPortInput();
+			void displayConnectionButton();
+			void detectActionMenu();
+			void mouseClickedMenu();
+			void keyPressededMenu(int KeyPressed);
+			void drawMenu();
+
+			void drawGame();
+
+
             Display();
             ~Display();
 
         protected:
         private:
+			std::string _HostName;
+			std::string _Port;
+			int _State = 0;
+			Rectangle _HostNameclickableZone;
+			Rectangle _PortclickableZone;
+			Rectangle _ConnectionclickableZone;
+			bool _GameState;
     };
 }
 
