542ff4518f8be9c1f25dd6b41aae78802932cd38
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-09-27 16:24:56 +0200

    feat: update main to use the client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -75,33 +75,49 @@ void initialiseWindow()
 	// SetExitKey(KEY_NULL); // TODO: uncomment this line to have access to the escape key
 }
 
-int main()
-{
-	initialiseWindow();
-	const int ballRadius = 10;
-	GameEngine::GameEngine engine;
-	engine.createRegistry("entities");
-	containers::Registry &registry = engine["entities"];
-	registry = setupRegistry(registry);
+// int main()
+// {
+// 	initialiseWindow();
+// 	const int ballRadius = 10;
+// 	GameEngine::GameEngine engine;
+// 	engine.createRegistry("entities");
+// 	containers::Registry &registry = engine["entities"];
+// 	registry = setupRegistry(registry);
 
-	Entity player = spawnShip(registry);
-	registry.emplaceComponent<components::ControllableComponent>(player);
-	registry.getComponents<components::PositionComponent>().at(player)->x = GetScreenWidth() / 2;
-	registry.getComponents<components::PositionComponent>().at(player)->y = GetScreenHeight() / 2;
+// 	Entity player = spawnShip(registry);
+// 	registry.emplaceComponent<components::ControllableComponent>(player);
+// 	registry.getComponents<components::PositionComponent>().at(player)->x = GetScreenWidth() / 2;
+// 	registry.getComponents<components::PositionComponent>().at(player)->y = GetScreenHeight() / 2;
 
-	for (int i = 0; i < 10; i++) {
-		Entity entity = spawnShip(registry);
-		registry.emplaceComponent<components::PositionComponent>(entity, GetRandomValue(ballRadius, GetScreenWidth() - ballRadius), GetRandomValue(ballRadius, GetScreenHeight() - ballRadius));
-		registry.emplaceComponent<components::VelocityComponent>(entity, GetRandomValue(-200, 200), GetRandomValue(-200, 200));
-		registry.getComponents<components::DrawableComponent>().at(entity)->frame = GetRandomValue(0, 6);
-	}
-	while (!WindowShouldClose()) {
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawText("move the player with arrow keys", 10, 10, 20, DARKGRAY);
-		registry.runSystems();
-		EndDrawing();
-	}
-	CloseWindow();
-	return 0;
+// 	for (int i = 0; i < 10; i++) {
+// 		Entity entity = spawnShip(registry);
+// 		registry.emplaceComponent<components::PositionComponent>(entity, GetRandomValue(ballRadius, GetScreenWidth() - ballRadius), GetRandomValue(ballRadius, GetScreenHeight() - ballRadius));
+// 		registry.emplaceComponent<components::VelocityComponent>(entity, GetRandomValue(-200, 200), GetRandomValue(-200, 200));
+// 		registry.getComponents<components::DrawableComponent>().at(entity)->frame = GetRandomValue(0, 6);
+// 	}
+// 	while (!WindowShouldClose()) {
+// 		BeginDrawing();
+// 		ClearBackground(RAYWHITE);
+// 		DrawText("move the player with arrow keys", 10, 10, 20, DARKGRAY);
+// 		registry.runSystems();
+// 		EndDrawing();
+// 	}
+// 	CloseWindow();
+// 	return 0;
+// }
+
+
+#include <boost/asio.hpp>
+#include "client/network/ClientNetwork.hpp"
+
+int main() {
+    boost::asio::io_service io_service;
+    std::string host = "192.168.122.1";
+    int port = 4848;
+    Network::ClientNetwork client(io_service, host, port);
+    client.start();
+	client.sendAction(Network::Action::SHOOT);
+	client.sendAction(Network::Action::DROP);
+    io_service.run();
+    return 0;
 }
