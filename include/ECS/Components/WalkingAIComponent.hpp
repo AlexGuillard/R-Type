e78d2aa700c7be98ffae5b72689cc7feb6fa25ff
azeuio <nicolas.mesrine@epitech.eu>
2023-10-08 17:24:58 +0200

    feat(System): make walking ai move only if they are grounded

--- include/ECS/Components/WalkingAIComponent.hpp
+++ include/ECS/Components/WalkingAIComponent.hpp
@@ -12,7 +12,7 @@
 namespace ECS::Components {
     /**
      * @brief Component owned by entities that have a target and need to move
-     * towards it.
+     * towards it. Note that the entity needs to be grounded to move.
      * @param preferredDistance Pair of floats representing the preferred
      * distance between the entity and its target (first: min, second: max)
      * @param speed Walking speed of the entity
