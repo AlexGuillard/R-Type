7c2ccd724b76cba631156f5791c5bb898df37207
azeuio <nicolas.mesrine@epitech.eu>
2023-10-08 15:32:03 +0200

    feat(Component): create the target component and system

---
+++ src/ECS/systems/target.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** target
+*/
+
+#include <cmath>
+#include <raylib.h>
+
+#include "ECS/Systems/target.hpp"
+#include "ECS/Containers/zipper/Zipper.hpp"
+
+namespace ECS::Systems {
+    void target(
+        Containers::Registry &registry,
+        Containers::SparseArray<Components::TargetComponent> &targets,
+        Containers::SparseArray<Components::PositionComponent> &positions)
+    {
+        for (auto &&[target, position] : Containers::Zipper(targets, positions)) {
+            auto &targetPosition = positions[target->targetId];
+            if (!targetPosition.has_value()) { continue; }
+            target->dX = targetPosition->x - position->x;
+            target->dY = targetPosition->y - position->y;
+            target->distance = std::sqrt(std::pow(target->dX, 2) + std::pow(target->dY, 2));
+            target->t += GetFrameTime();
+        }
+    }
+}
--- src/GameEngine/createEngine.cpp
+++ src/GameEngine/createEngine.cpp
@@ -21,12 +21,14 @@
 #include "ECS/Components/CollidableComponent.hpp"
 #include "ECS/Components/CollisionComponent.hpp"
 #include "ECS/Components/SinMovementComponent.hpp"
+#include "ECS/Components/TargetComponent.hpp"
 #include "ECS/Systems/controller.hpp"
 #include "ECS/Systems/movement.hpp"
 #include "ECS/Systems/drawable.hpp"
 #include "ECS/Systems/shooting.hpp"
 #include "ECS/Systems/collision.hpp"
 #include "ECS/Systems/sinMovement.hpp"
+#include "ECS/Systems/target.hpp"
 
 namespace GameEngine {
     namespace Containers = ECS::Containers;
@@ -47,6 +49,7 @@ namespace GameEngine {
         registry.registerComponent<Components::CollidableComponent>();
         registry.registerComponent<Components::CollisionComponent>();
         registry.registerComponent<Components::SinMovementComponent>();
+        registry.registerComponent<Components::TargetComponent>();
 
         registry.addSystem<Components::PositionComponent, Components::VelocityComponent, Components::ControllableComponent>(Systems::controller);
         registry.addSystem<Components::PositionComponent, Components::VelocityComponent>(Systems::movement);
@@ -54,6 +57,8 @@ namespace GameEngine {
         registry.addSystem<Components::MissileComponent, Components::WaveBeamComponent>(Systems::shooting);
         registry.addSystem<Components::PositionComponent, Components::HitBoxComponent, Components::CollidableComponent, Components::CollisionComponent>(Systems::collision);
         registry.addSystem<Components::SinMovementComponent, Components::PositionComponent>(Systems::sinMovement);
+        registry.addSystem<Components::TargetComponent, Components::PositionComponent>(Systems::target);
+        registry.addSystem<Components::TargetComponent, Components::PositionComponent, Components::VelocityComponent>(moveTowardTarget);
     }
 
     static ECS::Entity spawnShip(Containers::Registry &registry)
@@ -63,14 +68,16 @@ namespace GameEngine {
         ECS::Entity ship = registry.spawnEntity();
         registry.emplaceComponent<Components::PositionComponent>(ship, 0, 0);
         registry.emplaceComponent<Components::VelocityComponent>(ship, 0, 0);
+        static int i = 0;
         Components::DrawableComponent drawableComponent = {
             "assets/r-typesheet42.gif",
             nbFrameInSpriteSheet, // frameRatio
-            Vector2(0, 0), // start
-            Vector2(nbFrameInAnimation, 0), // end
+            Vector2(0 + nbFrameInAnimation * i, 0), // start
+            Vector2(nbFrameInAnimation + nbFrameInAnimation * i, 0), // end
             true, // boomerang
             nbFrameInAnimation // fps
         };
+        i = (i + 1) % 5;
         registry.addComponent<Components::DrawableComponent>(ship, std::move(drawableComponent));
         registry.emplaceComponent<Components::CollidableComponent>(ship);
         const Vector2 hitboxSize = Vector2(32, 16);
@@ -84,6 +91,8 @@ namespace GameEngine {
         registry.emplaceComponent<Components::ControllableComponent>(player);
         registry.getComponents<Components::PositionComponent>().at(player)->x = GetScreenWidth() / 2;
         registry.getComponents<Components::PositionComponent>().at(player)->y = GetScreenHeight() / 2;
+        ECS::Entity other = spawnShip(registry);
+        registry.emplaceComponent<Components::TargetComponent>(other, player);
     }
 
     GameEngine createEngine()
