4a46fea33ffa80e7969263b40a28e7d12ffab929
azeuio <nicolas.mesrine@epitech.eu>
2023-10-14 11:53:21 +0200

    fix(System): allow damage system to make mutual destruction

--- src/ECS/systems/damage.cpp
+++ src/ECS/systems/damage.cpp
@@ -21,19 +21,26 @@ namespace ECS::Systems {
         ECS::Containers::SparseArray<ECS::Components::TeamComponent> &teams,
         ECS::Containers::SparseArray<ECS::Components::HPComponent> &hpComponents)
     {
+        std::vector<std::size_t> toDelete;
+
         for (auto &&[entityId, hpComponent, collision] : Containers::IndexedZipper(hpComponents, collision)) {
             auto &&team = teams.at(entityId);
             for (auto eId : collision->collisions) {
                 if (!team || !teams.at(eId) || teams.at(eId)->team != team->team) {
+                    if (!damage.at(eId)) { continue; } // ignore entities that can't damage
                     std::size_t damageOther = damage.at(eId)->damage;
                     std::size_t hpEntity = hpComponents.at(entityId)->hp;
                     if (hpEntity == 0 || hpEntity <= damageOther) {
-                        registry.killEntity(registry.entityFromIndex(entityId));
+                        // store the entity to allow mutual destruction
+                        toDelete.push_back(entityId);
                         break;
                     }
                     hpComponents.at(entityId)->hp -= damageOther;
                 }
             }
+        }
+        for (auto &&entityId : toDelete) {
+            registry.killEntity(registry.entityFromIndex(entityId));
         }
     }
 
