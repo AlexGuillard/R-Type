4185a233e796654cc1dc605e2ee3be642763afdc
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-10-25 12:49:05 +0200

    fix(Display.cpp): add verif port / hostname void

--- src/client/display/Display.cpp
+++ src/client/display/Display.cpp
@@ -297,8 +297,10 @@ void Screen::Display::mouseClickedMenu()
         _state = InputState::NONE;
     }
     if (CheckCollisionPointRec(mouse, _connectionclickableZone)) {
-        std::cout << "\n Try Connexion\nwith:" << _hostName << " | " << _port << "\n\n";
-        _menuState = MenuState::CONNECTING;
+        if (_port != "" && _hostName != "") {
+            std::cout << "\n Try Connexion\nwith:" << _hostName << " | " << _port << "\n\n";
+            _menuState = MenuState::CONNECTING;
+        }
     }
 }
 
