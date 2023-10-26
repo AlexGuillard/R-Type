07f9d48661e37bfa9a3fb292eeb0e30f3ac53e25
Kiya971 <kiya.kebaili@epitech.eu>
2023-10-26 21:08:49 +0200

    feat(Component): add compjnent ground and ground displau

--- src/GameEngine/createClientEngine.cpp
+++ src/GameEngine/createClientEngine.cpp
@@ -9,11 +9,13 @@
 
 #include "GameEngine/GameEngine.hpp"
 #include "ECS/Containers/Registry.hpp"
+#include "ECS/Components/GroundComponent.hpp"
 #include "ECS/Components/BackgroundComponent.hpp"
 #include "ECS/Components/LevelComponent.hpp"
 #include "ECS/Systems/controller.hpp"
 #include "ECS/Systems/drawable.hpp"
 #include "ECS/Systems/background.hpp"
+#include "ECS/Systems/ground.hpp"
 #include "Assets/generatedAssets.hpp"
 #include "client/display/Display.hpp"
 #include "Assets/AssetLoader.hpp"
@@ -35,8 +37,10 @@ namespace GameEngine {
         registry.registerComponent<Components::BackgroundComponent>();
         registry.registerComponent<Components::LevelComponent>();
         registry.registerComponent<Components::PositionComponent>();
+        registry.registerComponent<Components::GroundComponent>();
 
         registry.addSystem<Components::PositionComponent, Components::LevelComponent, Components::BackgroundComponent>(Systems::background);
+        registry.addSystem<Components::PositionComponent, Components::LevelComponent, Components::GroundComponent>(Systems::ground);
     }
     static Components::BackgroundComponent createBackground(Assets::AssetsIndex index, std::vector<double> sizeSpeed)
     {
@@ -48,6 +52,18 @@ namespace GameEngine {
         };
         return backgroundComponent;
     }
+
+    static Components::GroundComponent createGround(Assets::AssetsIndex index, std::vector<double> sizeSpeed)
+    {
+        Components::GroundComponent groundComponent = {
+            index,
+            sizeSpeed[0],
+            sizeSpeed[1],
+            sizeSpeed[2]
+        };
+        return groundComponent;
+    }
+
 
     static void populateEntities(Containers::Registry &registry)
     {}
@@ -71,10 +87,13 @@ namespace GameEngine {
         for (size_t i = 0; i < assetIndices.size(); ++i)
         {
             Components::BackgroundComponent background = createBackground(assetIndices[i], sizeAndSpeed[i]);
+            Components::GroundComponent ground = createGround(Assets::AssetsIndex::GROUND1_PNG,  {double(5.0), double(1.0), double(0.020)});
             ECS::Entity level = registry.spawnEntity();
             registry.emplaceComponent<Components::BackgroundComponent>(level, background);
             registry.emplaceComponent<Components::LevelComponent>(level, _level);
             registry.emplaceComponent<Components::PositionComponent>(level, 0, 0);
+            registry.emplaceComponent<Components::GroundComponent>(level, ground);
+            registry.emplaceComponent<Components::PositionComponent>(level, 0, 950);
         }
     }
 
